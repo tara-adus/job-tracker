554f36f7719be7ea42857c706dbb446f5f902230
Tara <tara.adusumilli@gmail.com>
2023-06-18 10:05:16 -0400

    invoke backend service

--- js/appmanager.js
+++ js/appmanager.js
@@ -1,4 +1,21 @@
 
 function addJob(){
     alert("addJob has been invoked!")
+
+    let endpoint = 'http://127.0.0.1:3000/applications'
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',endpoint);
+    xhr.send()
+
+    xhr.onload = function() {
+        //let jsonresponse = JSON.parse(xhr.response)
+        //alert('sunrise:'+ xhr.status+ jsonresponse.results.sunrise)
+        alert(`Received response: ${xhr.status} ${xhr.response}`);
+        //document.getElementById("blah").innerHTML = this.response;
+    };
+
+    xhr.onerror = function() {
+        alert(`Network Error`);
+    };
 }
