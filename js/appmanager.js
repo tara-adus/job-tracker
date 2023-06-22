35d94a88aefafa5473cfab45fead9f2c2c5f9c2f
Tara <tara.adusumilli@gmail.com>
2023-06-22 12:59:15 -0400

    Call the backend

--- js/appmanager.js
+++ js/appmanager.js
@@ -2,20 +2,31 @@
 function addJob(){
     alert("addJob has been invoked!")
 
-    let endpoint = 'http://127.0.0.1:3000/applications'
+    let jobId = document.getElementById("jobId").value
+    let cname = document.getElementById("cname").value
+    console.log('adding job: '+ jobId+ ', '+ cname)
+    let endpoint = 'http://127.0.0.1:3000/application'
+
+    let jobApplication = {jobId, cname}
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET',endpoint);
-    xhr.send()
+   // xhr.withCredentials = true; //For CORS requests
+    xhr.open('POST',endpoint);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    //xhr.responseType = 'json';
+    
+    xhr.send(JSON.stringify(jobApplication))
 
     xhr.onload = function() {
         //let jsonresponse = JSON.parse(xhr.response)
         //alert('sunrise:'+ xhr.status+ jsonresponse.results.sunrise)
         alert(`Received response: ${xhr.status} ${xhr.response}`);
+        console.log('xhr.response='+xhr.response)
         //document.getElementById("blah").innerHTML = this.response;
     };
 
     xhr.onerror = function() {
+        console.log("error")
         alert(`Network Error`);
     };
 }
