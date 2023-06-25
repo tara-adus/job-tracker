c34c1e8a7241cb326db3378c4d36055d2de4b5df
Tara <tara.adusumilli@gmail.com>
2023-06-24 20:08:29 -0400

    get all job applications

--- js/appmanager.js
+++ js/appmanager.js
@@ -31,14 +31,41 @@ function addJobApplication(){
     };
 }
 
-function addJobApplications(){
+function getJobApplications(){
+    let endpoint = 'http://127.0.0.1:3000/applications'
+    let jobApplication = {jobId, cname}
+
+    const xhr = new XMLHttpRequest();
+   // xhr.withCredentials = true; //For CORS requests
+    xhr.open('GET',endpoint);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    //xhr.responseType = 'json';
+    xhr.send()
+
+    xhr.onload = function() {
+        let jobApplications = JSON.parse(xhr.response);
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+        for (let jobA of jobApplications){
+            out = out + `<tr><td>${jobA.jobId}</td><td>${jobA.cname}</td></tr>`
+        }
+        placeholder.innerHTML = out;
+    };
+
+    xhr.onerror = function() {
+        console.log("error")
+        alert(`Network Error`);
+    };
+}
+
+function addJobApplication(){
     alert("addJob has been invoked!")
 
     let jobId = document.getElementById("jobId").value
     let cname = document.getElementById("cname").value
     console.log('adding job: '+ jobId+ ', '+ cname)
-    let endpoint = 'http://127.0.0.1:3000/application'
 
+    let endpoint = 'http://127.0.0.1:3000/application'
     let jobApplication = {jobId, cname}
 
     const xhr = new XMLHttpRequest();
